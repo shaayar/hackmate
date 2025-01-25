ae17b95836a9651fb8b16b48778a66c786e831c5
zays21 <ankesstupid@gmail.com>
2025-01-25 16:54:25 +0530

    creating controller and route

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 const connect = require('./config/db.connect')
+const userRoutes = require("./routes/userRoutes")
 
 connect()
 
 app.use(express.json())
+app.use(userRoutes)
 
 
 const PORT = process.env.PORT || 5000;
